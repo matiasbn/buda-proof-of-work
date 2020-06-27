2cafcc5182990bf639ba39ed8eefd59ec0624e02
Matías Barrios <matias.barriosn@gmail.com>
2020-06-27 11:49:14 -0400

    creating a json of the data

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 #Buda POW
 
-JS script to lookup for the 'b00da' substring in a random 100 length long hex string.
+JS script to lookup for the 'b00da' substring in the SHA256 digest of a random 100 length long hex string.
+
+Then add the input, the digest of SHA256 and the position of the 'b00da' substring to an array of objects.
--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 const sha256 = require('crypto-js/sha256');
 const cryptoRandomString = require('crypto-random-string');
+const fs = require('fs');
 
 let code = cryptoRandomString({ length: 100 });
 let hash = sha256(code).toString();
 const digests = [];
-while (digests.length !== 10) {
+
+while (digests.length !== 20) {
   if (hash.indexOf('b00da') !== -1) {
     digests.push({ code, hash, position: hash.indexOf('b00da') });
   }
   code = cryptoRandomString({ length: 100 });
   hash = sha256(code).toString();
 }
-console.log(digests);
+
+const object = {};
+digests.forEach((digest, index) => {
+  object[index + 1] = digest;
+});
+
+const json = JSON.stringify(object);
+
+console.log(object);
+console.log(json);
+
+fs.writeFile('buda-pow.json', json, 'utf8', () => console.log('done'));
